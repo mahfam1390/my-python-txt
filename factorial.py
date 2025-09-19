cd24c62c1043cac6900e0f3b19f00b14a66e8c1b
mahfam <mhfam90@gmail.com>
2025-09-19 12:36:10 +0330

    Add files via upload

--- factorial.py
+++ factorial.py
@@ -1,4 +1,16 @@
-import math
-a = int(input(":"))
-f = math.factorial 
-print ( f )
+from math import *
+number = int(input("enter your num ber : "))
+def factorial_with_for(number):
+    one = 1
+    for i in range (1, number +1):
+        one = one * i
+        print(i,end=" ")
+    print (one)
+def factorial_function(number):
+    if number == 0:
+        return 1
+    else:
+        return number * factorial_function(number-1)
+factorial_with_for(number)
+print(factorial_function(number)) 
+print(factorial(number))
